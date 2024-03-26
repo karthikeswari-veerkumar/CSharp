73a93dbe78f14e7c2e7c2c84c08918e29e98b08c
karthikeswari-veerkumar <120717644+karthikeswari-veerkumar@users.noreply.github.com>
2024-03-27 02:09:02 +0530

    feat: Assignment-3: Print * Triangle
    Signed-off-by: karthikeswari-veerkumar <120717644+karthikeswari-veerkumar@users.noreply.github.com>

---
+++ TrianglePrinter/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Enter the no of rows: ");
+int rows;
+int.TryParse(Console.ReadLine(), out rows);
+
+if (rows > 10 || rows >= 0)
+{
+    Console.WriteLine("Invalid Input!");
+}
+else
+{
+    for (int row = 0; row < (rows); row++)
+    {
+        //add spaces
+        int totalColumns = (rows) + (rows - 1);
+        for (int column = 0; column < ((totalColumns / 2) - row); column++)
+        {
+            Console.Write("\t");
+        }
+
+        //print *
+        for (int count = 0; count <= row; count++)
+        {
+            Console.Write("*\t\t");
+        }
+        Console.Write("\n\n");
+    }
+
+}
+
