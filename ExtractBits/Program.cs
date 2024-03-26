153566140e870645252659f3cd2e29ac2e060417
karthikeswari-veerkumar <120717644+karthikeswari-veerkumar@users.noreply.github.com>
2024-03-27 01:40:47 +0530

    feat: Assignment-1: Extract Bits
    Signed-off-by: karthikeswari-veerkumar <120717644+karthikeswari-veerkumar@users.noreply.github.com>

---
+++ ExtractBits/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+int number;
+Console.Write("Enter the input number : ");
+int.TryParse(Console.ReadLine(), out number);
+Console.Write("Enter the Start Bit : ");
+int.TryParse(Console.ReadLine(), out int startBit);
+Console.Write("Enter the Stop Bit : ");
+int.TryParse(Console.ReadLine(), out int stopBit);
+
+string binary = Convert.ToString(number, 2);
+string reversedBinary = GetReversedBinary(binary);
+string extractedBinary = GetExtractedBinary(reversedBinary, startBit, stopBit);
+
+Console.WriteLine($"\nExtracted Binary Value : {extractedBinary}");
+Console.WriteLine($"Decimal Value : {Convert.ToInt32(extractedBinary, 2)}");
+
+string GetReversedBinary(string binary)
+{
+    char[] binaryArray = binary.ToCharArray();
+    Array.Reverse(binaryArray);
+    return new string(binaryArray);
+}
+
+string GetExtractedBinary(string binary, int startBit, int stopBit)
+{
+    string extractedBinary = binary.Substring(startBit, stopBit - startBit + 1);
+    char[] extractedBinArray = extractedBinary.ToCharArray();
+    Array.Reverse(extractedBinArray);
+    return new string(extractedBinArray);
+}
