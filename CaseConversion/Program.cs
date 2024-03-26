f9796ca9f00eb4ca5e28c34d127bdb58d77895cf
karthikeswari-veerkumar <120717644+karthikeswari-veerkumar@users.noreply.github.com>
2024-03-27 01:47:37 +0530

    feat: Assignment-2: Upper Case Conversion
    Signed-off-by: karthikeswari-veerkumar <120717644+karthikeswari-veerkumar@users.noreply.github.com>

---
+++ CaseConversion/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Case Conversion");
+string inputMessage;
+do
+{
+    Console.Write("\nEnter the String (type stop to exit) : ");
+    inputMessage = Console.ReadLine();
+    if (inputMessage != "stop")
+        Console.WriteLine($"Case converted string is {inputMessage.ToUpper()}");
+} while (inputMessage != "stop");
+
