b435fc57f7bbb39222600d1e3a15902e15920f8f
karthikeswari-veerkumar <120717644+karthikeswari-veerkumar@users.noreply.github.com>
2024-03-27 11:46:12 +0530

    feat: Assignment-1: Check for Consecutive Numbers
    Signed-off-by: karthikeswari-veerkumar <120717644+karthikeswari-veerkumar@users.noreply.github.com>

---
+++ ConsecutiveNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Enter the numbers : ");
+string inputNumbers = Console.ReadLine();
+
+string[] numbers = inputNumbers.Split("-");
+
+if (CheckForConsecutiveNumbers(numbers))
+{
+    Console.WriteLine("Consecutive!");
+}
+else
+{
+    Console.WriteLine("Not Consecutive!");
+}
+
+bool CheckForConsecutiveNumbers(string[] numbers)
+{
+    bool consecutive = true;
+    for (int index = 0; index < numbers.Length - 1; index++)
+    {
+        int number1 = Convert.ToInt32(numbers[index]);
+        int number2 = Convert.ToInt32(numbers[index + 1]);
+        int difference = number1 > number2 ? (number1 - number2) : (number2 - number1);
+        if (difference != 1)
+        {
+            consecutive = false;
+        }
+    }
+
+    return consecutive;
+}
+
